3107f661eaf02d74d56e5ea8943c6dcd639adc65
Alexandre Marcolino <marcolino.alexandre@gmail.com>
2025-03-14 21:52:04 -0300

    Upload Site Portfolio v1.0.0

---
+++ script.js
@@ -0,0 +1,6 @@
+// Validação do formulário de contato
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Mensagem enviada com sucesso!');
+    // Aqui você pode adicionar a lógica para enviar o formulário
+});
